1c794403aadc2dc6a533f8247246e37886071fd0
Fitzgerald Christian <150049668@cart.ads>
2018-03-22 13:56:48 -0700

    fixed slideshow, now I just need to make the text work better, maybe make the images bigger, found new images and cleaned up code a bit

--- website/main.js
+++ website/main.js
@@ -1,13 +1,15 @@
 // JavaScript for any pages that need it
-// This is for the slideshow
 
+// This is for the slideshow
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n){
+// Next/previous controls
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -16,13 +18,15 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none"; 
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
   dots[slideIndex-1].className += " active";
